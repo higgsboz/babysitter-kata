247aa7a7d50843d784f7db3cc9972263598ad6fb
higgsboz-beam <brandon.minner@beam.dental>
2023-02-08 21:41:51 -0500

    Add tests for dateUtil parser

---
+++ spec/dateUtils.spec.ts
@@ -0,0 +1,32 @@
+import { parseDate } from '../src/dateUtils'
+
+describe('src/dateUtils', () => {
+  describe('parseDate', () => {
+    it.each<string>([
+      '3:33am',
+      '3:33pm',
+      '7:30am',
+      '7:30pm',
+      '9:30am',
+      '9:30pm',
+      '11:11am',
+      '11:11pm',
+      '12:00am',
+      '12:00pm',
+    ])('should throw an error for invalid date: %s', (date) => {
+      const parsedDate = parseDate(date)
+      expect(parsedDate?.isValid()).toBeTruthy()
+    })
+
+    it.each<string>([
+      '9:30 pm',
+      '09:30pm',
+      '21:30pm',
+      '10:300pm',
+      '',
+      'gibberish',
+    ])('should throw an error for invalid date: %s', (date) => {
+      expect(() => parseDate(date)).toThrow(Error('Invalid Date'))
+    })
+  })
+})
